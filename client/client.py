8a3dc201a206a0223f3f5dc921ba7247e3376126
HensonGuo <1851228745@qq.com>
2021-11-22 20:56:01 +0800

    一个服务端+多个客户端

--- client/client.py
+++ client/client.py
@@ -4,14 +4,11 @@ client = socket.socket()             #创建一个客户端
 client.connect(('127.0.0.1', 8008))  #连接服务端
 
 while True:
-    s_info = client.recv(1024)           #接受服务端发来的消息
-    s_info = s_info.decode(encoding='utf-8')
+    info = input('>>>').encode('utf-8')
+    client.send(b'from c1:' + info)
+    s_info = client.recv(1024).decode(encoding='utf-8')
     print(s_info)
     if s_info == 'bye':  # 如果服务端发送的消息为bye，回复bye，结束循环
         client.send(b'bye')
-        print('bye_1')
         break
-    info = input('>>>')  # 输入数据，编码并发送给服务端
-    info = info.encode('utf-8')
-    client.send(info)
 client.close()                       #关闭客户端
--- server/server.py
+++ server/server.py
@@ -5,15 +5,16 @@ server.setblocking(True)         #解决服务重启时报address already in use
 server.bind(('127.0.0.1', 8008)) #设置服务端的ip和端口号
 server.listen()                  #开始监听
 
-conn, addr = server.accept()     #接受服务器端发出的消息和地址信息
 while True:
-    info = input(">>>")
-    info = info.encode("utf-8")
-    conn.send(info)
-    c_info = conn.recv(1024).decode('utf-8')         #接受客户端消息并解码
-    print(c_info)
-    if c_info == 'bye':  # 当客户端发送bye时，服务端给客户端发送bye并结束循环
-        conn.send(b'bye')
-        break
-conn.close()                     #关闭连接
-server.close()                   #关闭服务端
+    conn, addr = server.accept()     #接受服务器端发出的消息和地址信息
+    while True:
+        c_info = conn.recv(1024).decode('utf-8')         #接受客户端消息并解码
+        print(c_info)
+        if c_info == 'bye':  # 当客户端发送bye时，服务端给客户端发送bye并结束循环
+            conn.send(b'bye')
+            break
+        info = input('>>>').encode('utf-8')
+        conn.send(info)
+    conn.close()                     #关闭连接
+
+# server.close()                   #关闭服务端
